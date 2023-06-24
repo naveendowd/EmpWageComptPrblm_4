8f3d01c7580ff3852583ce703c09259124ca36df
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-06-24 18:34:56 +0530

    Refactored the code to add CompanyManager class and improve … …wage calculation and storage

---
+++ EmpWageCompPrblm3/CompanyManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_EmployeeWageComputation
+{
+    internal class CompanyManager
+    {
+        public List<CompanyDetails> listOfCompanies;
+
+        public CompanyManager()
+        {
+            listOfCompanies = new List<CompanyDetails>();
+        }
+
+        public void AddCompanyDetails(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+        {
+            CompanyDetails comp = new CompanyDetails(company, empRatePerHour, noOfWorkingDays, maxHrsPerMonth);
+            listOfCompanies.Add(comp);
+        }
+    }
+}
