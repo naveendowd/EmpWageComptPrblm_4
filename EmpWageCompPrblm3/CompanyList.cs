c87148575301d082454cf12128bb05fe0b8da125
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-06-24 18:26:07 +0530

    Company Details Stored IN Array

---
+++ EmpWageCompPrblm3/CompanyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_EmployeeWageComputation
+{
+    internal class CompanyDetails
+    {
+        public string company;
+        public int empRatePerHour, noOfWorkingDays, maxHrsPerMonth, totalWage;
+
+        public CompanyDetails(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.noOfWorkingDays = noOfWorkingDays;
+            this.maxHrsPerMonth = maxHrsPerMonth;
+        }
+
+        public void SetTotalEmpWage(int totalEmpWage)
+        {
+            totalWage = totalEmpWage;
+        }
+    }
+}
